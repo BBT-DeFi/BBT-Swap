d927490609a2bc8398f0d952f85899bf88a7aabc
Thanakait Homhual <61160061@go.buu.ac.th>
2021-07-23 03:50:39 +0700

    Add favicon // Edit Title page to BBTSwap

--- frontend/src/config/constants/meta.ts
+++ frontend/src/config/constants/meta.ts
@@ -12,47 +12,47 @@ export const getCustomMeta = (path: string, t: ContextApi['t']): PageMeta => {
   switch (path) {
     case '/':
       return {
-        title: `${t('Home')} | ${t('PancakeSwap')}`,
+        title: `${t('Home')} | ${t('BBTSwap')}`,
       }
     case '/competition':
       return {
-        title: `${t('Trading Battle')} | ${t('PancakeSwap')}`,
+        title: `${t('Trading Battle')} | ${t('BBTSwap')}`,
       }
     case '/prediction':
       return {
-        title: `${t('Prediction')} | ${t('PancakeSwap')}`,
+        title: `${t('Prediction')} | ${t('BBTSwap')}`,
       }
     case '/farms':
       return {
-        title: `${t('Farms')} | ${t('PancakeSwap')}`,
+        title: `${t('Farms')} | ${t('BBTSwap')}`,
       }
     case '/pools':
       return {
-        title: `${t('Pools')} | ${t('PancakeSwap')}`,
+        title: `${t('Pools')} | ${t('BBTSwap')}`,
       }
     case '/lottery':
       return {
-        title: `${t('Lottery')} | ${t('PancakeSwap')}`,
+        title: `${t('Lottery')} | ${t('BBTSwap')}`,
       }
     case '/collectibles':
       return {
-        title: `${t('Collectibles')} | ${t('PancakeSwap')}`,
+        title: `${t('Collectibles')} | ${t('BBTSwap')}`,
       }
     case '/ifo':
       return {
-        title: `${t('Initial Farm Offering')} | ${t('PancakeSwap')}`,
+        title: `${t('Initial Farm Offering')} | ${t('BBTSwap')}`,
       }
     case '/teams':
       return {
-        title: `${t('Leaderboard')} | ${t('PancakeSwap')}`,
+        title: `${t('Leaderboard')} | ${t('BBTSwap')}`,
       }
     case '/profile/tasks':
       return {
-        title: `${t('Task Center')} | ${t('PancakeSwap')}`,
+        title: `${t('Task Center')} | ${t('BBTSwap')}`,
       }
     case '/profile':
       return {
-        title: `${t('Your Profile')} | ${t('PancakeSwap')}`,
+        title: `${t('Your Profile')} | ${t('BBTSwap')}`,
       }
     default:
       return null
